16a43b713daff2d2b70f9f839a4fe9ac5b929a19
mrkasore <mrkasore@gmail.com>
2023-10-23 22:28:15 +0300

    Add a pen

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
-const body = document.querySelector('body');
+const container = document.querySelector('.container');
+let isPressed = false;
 
 //Create grid
 for (let i = 0; i < 16; i++) {
     let div = document.createElement('div');
+    div.setAttribute('class', 'row');
     for (let y = 0; y < 16; y++) {
         let divIn = document.createElement('div');
         divIn.setAttribute('class', 'cell');
         div.appendChild(divIn);
     }
-    body.appendChild(div);
+    container.appendChild(div);
 }
 
 
 const allCells = document.querySelectorAll('.cell');
 
 allCells.forEach(item => {
-    item.addEventListener('click', function(e) {
-        console.log(e.target);
+    item.addEventListener('mousedown', function(e) {
+        //console.log(e.target);
+            //e.target.style.backgroundColor = 'black';
+        
     })
-})
+});
+
+container.addEventListener('mousedown', function(e) {
+    isPressed = true;
+}); 
+
+container.addEventListener('mouseup', function(e) {
+    isPressed = false;
+}); 
+
+container.addEventListener('mousemove', function(e) {
+    if (isPressed && e.target.className == 'cell') {
+        e.target.style.backgroundColor = 'black';
+    } 
+});
+
+container.addEventListener('click', function(e) {
+    if (e.target.className == 'cell') {
+        e.target.style.backgroundColor = 'black';
+    }
+    
+});
+
