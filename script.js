0b95f4ae8642a6ebe28785bee0b484a0809aadce
mrkasore <mrkasore@gmail.com>
2023-10-24 23:13:35 +0300

    Add all features

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const btnGrid = document.querySelector('.gridButton');
 const inp = document.querySelector('#inp');
 
 
+document.querySelector('.colors').style.backgroundColor = 'black';
 
 //Create grid
 function createGrid(num) {
@@ -21,27 +22,29 @@ function createGrid(num) {
 }
 
 
-
-const allCells = document.querySelectorAll('.cell');
+const allCells = document.querySelectorAll('.cell');;
 
 // Mouse check
 container.addEventListener('mousedown', function(e) {
     isPressed = true;
+    e.preventDefault();
 }); 
 
-container.addEventListener('mouseup', function(e) {
+body.addEventListener('mouseup', function(e) {
+    console.log('false');
+    e.preventDefault();
     isPressed = false;
 }); 
 
 container.addEventListener('mousemove', function(e) {
     if (isPressed && e.target.className == 'cell') {
-        e.target.style.backgroundColor = 'black';
+        e.target.style.backgroundColor = document.querySelector('.colors').style.backgroundColor;
     } 
 });
 
 container.addEventListener('click', function(e) {
     if (e.target.className == 'cell') {
-        e.target.style.backgroundColor = 'black';
+        e.target.style.backgroundColor = document.querySelector('.colors').style.backgroundColor;
     }
     
 });
@@ -49,8 +52,14 @@ container.addEventListener('click', function(e) {
 
 
 btnGrid.addEventListener('click', function(e) {
-    console.log(inp.value);
+    
     deleteItems();
+    if(inp.value >= 100) {
+        inp.value = 100;
+    } else if (inp.value <= 0) {
+        inp.value = 1;
+    }
+    console.log(inp.value);
     createGrid(inp.value);
 });
 
@@ -61,3 +70,22 @@ function deleteItems() {
     }
   }
 createGrid(16);
+
+
+
+document.querySelector('#red').addEventListener('input', changeColor);
+document.querySelector('#green').addEventListener('input', changeColor);
+document.querySelector('#blue').addEventListener('input', changeColor);
+
+
+//Change color
+function changeColor() {
+    let red = document.querySelector('#red').value;
+    let green = document.querySelector('#green').value;
+    let blue = document.querySelector('#blue').value;
+    document.querySelector('.colors').style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+}
+//Eraser
+document.querySelector('.eraser').addEventListener('click', function() {
+    document.querySelector('.colors').style.backgroundColor = 'white';
+});
